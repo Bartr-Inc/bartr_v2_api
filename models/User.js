40d981a9d8538aae0b78c133e7fdef9a034c9913
Kusjay <kelechiuma@gmail.com>
2023-06-16 12:23:56 +0100

    Added verify user OTP on reset password

--- models/User.js
+++ models/User.js
@@ -4,55 +4,63 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true, 'Please add a full name'],
-  },
-  phone: {
-    type: Number,
-    required: [true, 'Please add a phone number'],
-  },
-  email: {
-    type: String,
-    required: [true, 'Please add an email'],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      'Please add a valid email',
-    ],
-    // unique: true,
-  },
-  role: {
-    type: String,
-    default: 'User',
-  },
-  password: {
-    type: String,
-    required: [true, 'Please add a password'],
-    minlength: 6,
-    select: false,
-  },
-  walletId: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Wallet',
-  },
-  subscriptionId: {
-    type: mongoose.Schema.ObjectId,
-    ref: 'Subscription',
-  },
-  otpCode: {
-    type: String,
-    default: null,
-  },
-  otpConfirmed: {
-    type: String,
-    default: 'No',
-  },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+	fullName: {
+		type: String,
+		required: [true, 'Please add a full name'],
+	},
+	phone: {
+		type: Number,
+		required: [true, 'Please add a phone number'],
+	},
+	email: {
+		type: String,
+		required: [true, 'Please add an email'],
+		match: [
+			/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+			'Please add a valid email',
+		],
+		// unique: true,
+	},
+	role: {
+		type: String,
+		default: 'User',
+	},
+	password: {
+		type: String,
+		required: [true, 'Please add a password'],
+		minlength: 6,
+		select: false,
+	},
+	walletId: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'Wallet',
+	},
+	subscriptionId: {
+		type: mongoose.Schema.ObjectId,
+		ref: 'Subscription',
+	},
+	otpCode: {
+		type: String,
+		default: null,
+	},
+	otpConfirmed: {
+		type: String,
+		default: 'False',
+	},
+	resetPasswordOtpCode: {
+		type: String,
+		default: null,
+	},
+	resetPasswordOtpConfirmed: {
+		type: String,
+		default: 'False',
+	},
+	resetPasswordToken: String,
+	resetPasswordExpire: Date,
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 // Cascade delete wallet when a user is deleted
@@ -62,41 +70,41 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
-  }
+	if (!this.isModified('password')) {
+		next();
+	}
 
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Sign JWT and return
 UserSchema.methods.getSignedJwtToken = function () {
-  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
+	return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRE,
+	});
 };
 
 // Match user entered password to hashed pasword in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
+	return await bcrypt.compare(enteredPassword, this.password);
 };
 
 // Generate and hash password token
 UserSchema.methods.getResetPasswordToken = function () {
-  // Generate token
-  const resetToken = crypto.randomBytes(20).toString('hex');
+	// Generate token
+	const resetToken = crypto.randomBytes(20).toString('hex');
 
-  // Hash token and set to resetPasswordToken field
-  this.resetPasswordToken = crypto
-    .createHash('sha256')
-    .update(resetToken)
-    .digest('hex');
+	// Hash token and set to resetPasswordToken field
+	this.resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(resetToken)
+		.digest('hex');
 
-  // Set expire
-  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+	// Set expire
+	this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
 
-  return resetToken;
+	return resetToken;
 };
 
 module.exports = mongoose.model('User', UserSchema);
